c2bac7cee8c173a5a0d5c8ef56d7b997132e9925
masajinobe-ef <priscilla.effects@gmail.com>
2024-01-02 17:25:48 +0500

    v0.6

--- prsc.py
+++ prsc.py
@@ -123,6 +123,7 @@ def master():
 
 
 if __name__ == '__main__':
-    # from waitress import serve
-    # serve(app, host="0.0.0.0", port=8080)
-    app.run(debug=True)
+    from waitress import serve
+    print("Запущен сервер!")
+    serve(app, host="0.0.0.0", port=8080)
+    # app.run(debug=True)
